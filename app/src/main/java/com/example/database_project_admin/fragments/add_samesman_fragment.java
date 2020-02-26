6302e211fe777213f82122f3f37de7b5dc2a2217
ahsanfarooq210 <ahsanfarooq210@gmail.com>
2020-02-26 14:07:12 +0500

    initial

---
+++ app/src/main/java/com/example/database_project_admin/fragments/add_samesman_fragment.java
@@ -0,0 +1,35 @@
+package com.example.database_project_admin.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.database_project_admin.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class add_samesman_fragment extends Fragment
+{
+
+
+    public add_samesman_fragment()
+    {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_samesman_fragment, container, false);
+    }
+
+}
