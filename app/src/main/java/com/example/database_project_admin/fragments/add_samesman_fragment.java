26959e06a2f65705f057866178adb12485b06cfc
ahsanfarooq210 <ahsanfarooq210@gmail.com>
2020-02-26 14:15:53 +0500

    initial

--- app/src/main/java/com/example/database_project_admin/fragments/add_samesman_fragment.java
+++ app/src/main/java/com/example/database_project_admin/fragments/add_samesman_fragment.java
@@ -3,13 +3,23 @@ package com.example.database_project_admin.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.database_project_admin.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +27,7 @@ import com.example.database_project_admin.R;
 public class add_samesman_fragment extends Fragment
 {
 
+    private FirebaseAuth mAuth;
 
     public add_samesman_fragment()
     {
@@ -30,6 +41,28 @@ public class add_samesman_fragment extends Fragment
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_samesman_fragment, container, false);
+    }
+
+    public void signupFunc(String email,String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
